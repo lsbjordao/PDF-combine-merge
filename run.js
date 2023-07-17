08bff6d0c7accb61c4e6cbd116e95fd7fd12aa53
lsbjordao <tucarj@gmail.com>
2023-07-17 17:21:54 -0300

    init

---
+++ run.js
@@ -0,0 +1,30 @@
+const { PDFDocument } = require('pdf-lib');
+const fs = require('fs');
+const path = require('path');
+
+async function combinePDFs() {
+  const pdfDoc = await PDFDocument.create();
+  const inputFolderPath = './input'; // Caminho para a pasta "input"
+
+  // Lê os nomes de todos os arquivos na pasta "input"
+  const fileNames = fs.readdirSync(inputFolderPath);
+
+  // Ordena os nomes dos arquivos em ordem alfabética
+  const sortedFileNames = fileNames.sort();
+
+  for (const fileName of sortedFileNames) {
+    const filePath = path.join(inputFolderPath, fileName);
+    const pdfBytes = fs.readFileSync(filePath);
+    const tempDoc = await PDFDocument.load(pdfBytes);
+    const copiedPages = await pdfDoc.copyPages(tempDoc, tempDoc.getPageIndices());
+    copiedPages.forEach((page) => pdfDoc.addPage(page));
+  }
+
+  const combinedPdfBytes = await pdfDoc.save();
+  const outputPath = './output/resultado.pdf'; // Caminho para o arquivo de saída combinado
+
+  fs.writeFileSync(outputPath, combinedPdfBytes);
+  console.log('Arquivos PDF combinados com sucesso!');
+}
+
+combinePDFs().catch((error) => console.error(error));
